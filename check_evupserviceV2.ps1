b22ad0af4c19af1ecdb030eaca398a5e5b4db734
Dennis Di Martini <dennismr.r@gmail.com>
2019-10-29 12:29:21 -0300

    Update check_evupserviceV2.ps1

--- check_evupserviceV2.ps1
+++ check_evupserviceV2.ps1
@@ -4,7 +4,7 @@ Start-Service EVUP
 $MyToken = (Get-Content c:\puppet\arquivos\tkn.txt)
 $chatID = -370673139
 $hostname = $env:COMPUTERNAME
-$Message = "Serviço Iniciado EVUP iniciado no host $hostname às $time"
+$Message = "Serviço EVUP iniciado no host $hostname às $time"
 $Response = Invoke-RestMethod -Uri "https://api.telegram.org/bot$($MyToken)/sendMessage?chat_id=$($chatID)&text=$($Message)"
 write-output "Serviço Iniciado - $time" >> c:\puppet\arquivos\log-check-service.log } else {
 write-host "Serviço em execução" }
